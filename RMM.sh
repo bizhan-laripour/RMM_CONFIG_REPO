148d7a3e1b846e8c8cc97c8deff4057b178cb853
Bizhan laripour <41441280+bizhan-laripour@users.noreply.github.com>
2024-03-02 02:32:58 +0330

    Update RMM.sh

--- RMM.sh
+++ RMM.sh
@@ -1,11 +1,9 @@
-bold=$(tput bold)  
-normal=$(tput sgr0)   
 NONE='\033[00m'  
 RED='\033[0;31m'    
 YELLOW='\033[01;33m'  
 GREEN='\033[01;32m'  
 echo -e
-${bold} ${NONE}Hello from ${RED}RMM${NONE}. Im ${GREEN}Bizhan Laripour ${NONE}and wish you enjoy with this project. 
+Hello from ${RED}RMM${NONE}. Im ${GREEN}Bizhan Laripour ${NONE}and wish you enjoy with this project. 
 
 "                                                    
 declare -a all=(   
